d1acb3e154221f659ca9f714908270f63f291e88
vasilisdimitriou <vasilisdmr@gmail.com>
2020-02-15 20:36:22 +0200

    Renamed the caller class and added the initializer within the deafult constructor.

---
+++ PeopleManagement/ApiCaller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using PersonRepository.Interface;
+using PersonRepository.Service;
+
+namespace PeopleManagement
+{
+    public class ApiCaller
+    {
+        IPersonRepository repository = new ServiceRepository();
+        public ApiCaller()
+        {
+            IEnumerable<Person> people = repository.GetPeople();
+        }
+    }
+}
