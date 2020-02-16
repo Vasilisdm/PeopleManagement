b03aa1b975d710407a9de8873564b99c090ae743
vasilisdimitriou <vasilisdmr@gmail.com>
2020-02-16 21:41:35 +0200

    Implemented the csv's repository get people method

--- PersonRepository.CSV/CSVRepository.cs
+++ PersonRepository.CSV/CSVRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using PersonRepository.Interface;
 
@@ -6,6 +7,14 @@ namespace PersonRepository.CSV
 {
     public class CSVRepository : IPersonRepository
     {
+        string path;
+
+        public CSVRepository()
+        {
+            var fileName = "test.csv";
+            path = AppDomain.CurrentDomain.BaseDirectory + fileName;
+        }
+
         public void AddPerson(Person newPerson)
         {
             throw new NotImplementedException();
@@ -18,7 +27,30 @@ namespace PersonRepository.CSV
 
         public IEnumerable<Person> GetPeople()
         {
-            throw new NotImplementedException();
+            var people = new List<Person>();
+
+            if (File.Exists(path))
+            {
+                using (var reader = new StreamReader(path))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        var elements = line.Split(',');
+                        var person = new Person()
+                        {
+                            Id = Int32.Parse(elements[0]),
+                            GivenName = elements[1],
+                            FamilyName = elements[2],
+                            StartDate = DateTime.Parse(elements[3]),
+                            Rating = Int32.Parse(elements[4]),
+                            FormatString = elements[5],
+                        };
+                        people.Add(person);
+                    }
+                }
+            }
+            return people;
         }
 
         public Person GetPerson(int id)
