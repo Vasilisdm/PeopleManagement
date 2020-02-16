d0f802694cbecb307a33b9f097a19a52c7ccb755
vasilisdimitriou <vasilisdmr@gmail.com>
2020-02-16 22:18:10 +0200

    Added factory repository .net library project alongisde with Repository factory class.

---
+++ PersonRepository.Factory/RepositoryFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using PersonRepository.CSV;
+using PersonRepository.Interface;
+using PersonRepository.Service;
+using PersonRepository.SQL;
+
+namespace PersonRepository.Factory
+{
+    public static class RepositoryFactory
+    {
+        public static IPersonRepository GetRepository(string repositoryType)
+        {
+            IPersonRepository repository = null;
+
+            switch (repositoryType)
+            {
+                case "Service": repository = new ServiceRepository();
+                    break;
+                case "CSV": repository = new CSVRepository();
+                    break;
+                case "SQl": repository = new SQLRepository();
+                    break;
+                default:
+                    throw new ArgumentException("Invalid repository type");
+            }
+
+            return repository;
+        }
+    }
+}
